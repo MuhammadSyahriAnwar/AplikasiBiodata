8e1aa2b7debe00c956a7a189c8c660ef746cf809
MuhammadSyahriAnwar <ahmad89.aa70@gmail.com>
2024-07-15 10:53:17 +0700

    Menambahkan fitur hapus mahasiswa

--- AplikasiBiodata.cpp
+++ AplikasiBiodata.cpp
@@ -23,4 +23,14 @@ public:
         data_mahasiswa.push_back(mahasiswa);
         cout << "Biodata mahasiswa " << nama << " berhasil ditambahkan." << endl;
     }
+void hapus_mahasiswa(string nim) {
+        for (auto it = data_mahasiswa.begin(); it != data_mahasiswa.end(); ++it) {
+            if (it->nim == nim) {
+                data_mahasiswa.erase(it);
+                cout << "Biodata mahasiswa dengan NIM " << nim << " berhasil dihapus." << endl;
+                return;
+            }
+        }
+        cout << "Mahasiswa dengan NIM " << nim << " tidak ditemukan." << endl;
+    }
 
